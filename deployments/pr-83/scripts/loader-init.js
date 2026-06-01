46842461571c8e4b224cea5a40fbf38201ef6b83
DerekRoberts <DerekRoberts@users.noreply.github.com>
2026-06-01 23:23:10 +0000

    Deploying to gh-pages from @ bcgov/nr-seedtransfer-map@829437880429edb1cd65e8d36fc7c9565bf91003 🚀

---
+++ deployments/pr-83/scripts/loader-init.js
@@ -0,0 +1,23 @@
+// Monkeypatch the missing reposition method on HTMLElement prototype to prevent
+// internal Calcite Components / Stencil asynchronous upgrade race condition crashes
+// where Esri CDN code calls popoverEl?.value?.reposition() before the element is fully upgraded.
+if (typeof HTMLElement.prototype.reposition !== 'function') {
+  HTMLElement.prototype.reposition = function () {
+    console.debug('Calcite Components custom element reposition placeholder called.')
+  }
+}
+
+;(function () {
+  var pathname = window.location.pathname
+  var locationPath = pathname.substring(0, pathname.lastIndexOf('/'))
+  window.dojoConfig = {
+    async: true,
+    cacheBust: 'v=7.0.6',
+    packages: [
+      {
+        name: 'scripts',
+        location: locationPath + '/scripts',
+      },
+    ],
+  }
+})()
