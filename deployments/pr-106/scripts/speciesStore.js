7b488be3040d414a1fd185c551445a2822212fd2
DerekRoberts <DerekRoberts@users.noreply.github.com>
2026-07-30 20:06:07 +0000

    Deploying to gh-pages from @ bcgov/nr-seedtransfer-map@572be00c75371096e97bdb84ec9fd831163cd40d 🚀

--- deployments/pr-106/scripts/speciesStore.js
+++ deployments/pr-106/scripts/speciesStore.js
@@ -8,7 +8,29 @@
   }
 })(this, function () {
   return [
+    { name: 'AT', minsuit: 97.5 },
+    { name: 'BA', minsuit: 97.5 },
+    { name: 'BG', minsuit: 98.5 },
+    { name: 'BL', minsuit: 97.0 },
+    { name: 'CW', minsuit: 98.0 },
+    { name: 'DR', minsuit: 97.5 },
+    { name: 'EP', minsuit: 97.5 },
+    { name: 'FDC', minsuit: 97.5 },
+    { name: 'FDI', minsuit: 97.5 },
+    { name: 'HM', minsuit: 97.5 },
+    { name: 'HW', minsuit: 97.5 },
+    { name: 'LT', minsuit: 97.5 },
+    { name: 'LW', minsuit: 97.5 },
+    { name: 'PA', minsuit: 96.5 },
+    { name: 'PJ', minsuit: 97.5 },
+    { name: 'PLC', minsuit: 97.5 },
     { name: 'PLI', minsuit: 97.5 },
+    { name: 'PW', minsuit: 96.0 },
+    { name: 'PY', minsuit: 96.0 },
+    { name: 'SB', minsuit: 97.5 },
+    { name: 'SS', minsuit: 97.0 },
     { name: 'SX', minsuit: 97.5 },
+    { name: 'SXS', minsuit: 97.5 },
+    { name: 'YC', minsuit: 96.0 },
   ]
 })
